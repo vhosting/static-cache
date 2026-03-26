8a0a8fe6f1a9f349f310c98ceaa15d3c9a54b94a
Luca Patera <lucapatera@outlook.it>
2026-03-26 16:16:59 +0100

    refactor: remove Welcome.vue and its dependencies

--- resources/js/pages/auth/Register.vue
+++ resources/js/pages/auth/Register.vue
@@ -1,15 +1,15 @@
 <script setup lang="ts">
-import { Form, Head } from '@inertiajs/vue3';
-import InputError from '@/components/InputError.vue';
-import PasswordInput from '@/components/PasswordInput.vue';
-import TextLink from '@/components/TextLink.vue';
-import { Button } from '@/components/ui/button';
-import { Input } from '@/components/ui/input';
-import { Label } from '@/components/ui/label';
-import { Spinner } from '@/components/ui/spinner';
+import { /*Form,*/ Head } from '@inertiajs/vue3';
+//import InputError from '@/components/InputError.vue';
+//import PasswordInput from '@/components/PasswordInput.vue';
+//import TextLink from '@/components/TextLink.vue';
+//import { Button } from '@/components/ui/button';
+//import { Input } from '@/components/ui/input';
+//import { Label } from '@/components/ui/label';
+//import { Spinner } from '@/components/ui/spinner';
 import AuthBase from '@/layouts/AuthLayout.vue';
-import { login } from '@/routes';
-import { store } from '@/routes/register';
+//import { login } from '@/routes';
+//import { store } from '@/routes/register';
 </script>
 
 <template>
@@ -19,7 +19,7 @@ import { store } from '@/routes/register';
     >
         <Head title="Register" />
 
-        <Form
+<!--        <Form
             v-bind="store.form()"
             :reset-on-success="['password', 'password_confirmation']"
             v-slot="{ errors, processing }"
@@ -102,6 +102,6 @@ import { store } from '@/routes/register';
                     >Log in</TextLink
                 >
             </div>
-        </Form>
+        </Form>-->
     </AuthBase>
 </template>
